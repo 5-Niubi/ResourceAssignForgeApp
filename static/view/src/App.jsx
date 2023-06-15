dbb027e87e678c5eb23b6620c5a561a416ce0955
quynm15 <97070841+quynm153283@users.noreply.github.com>
2023-06-15 22:26:57 +0700

    test page layout and main nav menu

--- static/view/src/App.jsx
+++ static/view/src/App.jsx
@@ -2,83 +2,132 @@
 import React, { useEffect, useState } from "react";
 import { invoke, router, view } from "@forge/bridge";
 import HomePage from "./pages/HomePage";
+import BasicExample from "./components/Nav";
 import { Route, Router, Routes, useNavigate } from "react-router";
 import ProjectFromNetPage from "./pages/ProjectFromNetPage";
 
+import {
+	LeftSidebarWithoutResize,
+	Main,
+	PageLayout,
+} from "@atlaskit/page-layout";
+
 function App() {
-  // Enable auto change theme Dark/light mode within Jira
-  view.theme.enable();
+	// Enable auto change theme Dark/light mode within Jira
+	view.theme.enable();
 
-  const [history, setHistory] = useState();
-  const [historyState, setHistoryState] = useState();
+	const [history, setHistory] = useState();
+	const [historyState, setHistoryState] = useState();
 
-  /**
-   * @param {string} authenUrl
-   */
-  async function handleAuthenOAuth(authenUrl) {
-    await router.open(authenUrl);
-  }
+	/**
+	 * @param {string} authenUrl
+	 */
+	async function handleAuthenOAuth(authenUrl) {
+		await router.open(authenUrl);
+	}
 
-  // Check authenticate every time reload page
-  useEffect(function () {
-    invoke("getAuthenUrl").then(function (res) {
-      if (!res.isAuthenticated) {
-        handleAuthenOAuth(res.authenUrl);
-      }
-    });
-  }, []);
+	// Check authenticate every time reload page
+	useEffect(function () {
+		invoke("getAuthenUrl").then(function (res) {
+			if (!res.isAuthenticated) {
+				handleAuthenOAuth(res.authenUrl);
+			}
+		});
+	}, []);
 
-  // // Set this app context to storage
-  // useEffect(() => {
-  //   invoke("setContextToGlobal").then().catch();
-  // }, []);
+	// // Set this app context to storage
+	// useEffect(() => {
+	//   invoke("setContextToGlobal").then().catch();
+	// }, []);
 
-  // --- Config React Router ---
-  useEffect(() => {
-    view.createHistory().then((newHistory) => {
-      setHistory(newHistory);
-    });
-  }, []);
+	// --- Config React Router ---
+	useEffect(() => {
+		view.createHistory().then((newHistory) => {
+			setHistory(newHistory);
+		});
+	}, []);
 
-  useEffect(() => {
-    if (!historyState && history) {
-      setHistoryState({
-        action: history.action,
-        location: history.location,
-      });
-    }
-  }, [history, historyState]);
+	useEffect(() => {
+		if (!historyState && history) {
+			setHistoryState({
+				action: history.action,
+				location: history.location,
+			});
+		}
+	}, [history, historyState]);
 
-  useEffect(() => {
-    if (history) {
-      history.listen((location, action) => {
-        setHistoryState({
-          action,
-          location,
-        });
-      });
-    }
-  }, [history]);
-  // --- / ---
+	useEffect(() => {
+		if (history) {
+			history.listen((location, action) => {
+				setHistoryState({
+					action,
+					location,
+				});
+			});
+		}
+	}, [history]);
+	// --- / ---
 
-  return (
-    <div>
-      {history && historyState ? (
-        <Router
-          navigator={history}
-          navigationType={historyState.action}
-          location={historyState.location}
-        >
-          <Routes>
-            <Route path="/" element={<HomePage />}></Route>
-            <Route path="/projects" element={<ProjectFromNetPage />}></Route>
-          </Routes>
-        </Router>
-      ) : (
-        "Loading..."
-      )}
-    </div>
-  );
+	// return (
+	//   <div>
+	//     {history && historyState ? (
+	//       <Router
+	//         navigator={history}
+	//         navigationType={historyState.action}
+	//         location={historyState.location}
+	//       >
+	//         <Routes>
+	//           <Route path="/" element={<BasicExample />}></Route>
+	//           <Route path="/projects" element={<ProjectFromNetPage />}></Route>
+	//         </Routes>
+	//       </Router>
+	//     ) : (
+	//       "Loading..."
+	//     )}
+	//   </div>
+	// );
+
+	return (
+		<PageLayout>
+			<LeftSidebarWithoutResize testId="leftSidebar" id="space-navigation">
+				{history && historyState ? (
+					<div style={{ height: "100vh" }}>
+						<Router
+							navigator={history}
+							navigationType={historyState.action}
+							location={historyState.location}
+						>
+							<Routes>
+								<Route path="/" element={<BasicExample />}></Route>
+							</Routes>
+						</Router>
+					</div>
+				) : (
+					"Loading..."
+				)}
+			</LeftSidebarWithoutResize>
+
+			{
+				<Main testId="main" id="main">
+					{history && historyState ? (
+						<div style={{ height: "100vh" }}>
+							<Router
+								navigator={history}
+								navigationType={historyState.action}
+								location={historyState.location}
+							>
+								<Routes>
+									<Route path="/" element={<HomePage />}></Route>
+								</Routes>
+							</Router>
+						</div>
+					) : (
+						"Loading..."
+					)}
+				</Main>
+			}
+		</PageLayout>
+	);
 }
 
 export default App;
