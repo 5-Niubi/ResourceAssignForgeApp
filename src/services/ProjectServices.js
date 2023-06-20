a63bbee7b96a45e60fa0bf59e017d7c460614c4d
area1110 <khanhhuy1110@gmail.com>
2023-06-20 11:31:05 +0700

    Move APIService to service/common, clean import

--- src/services/ProjectServices.js
+++ src/services/ProjectServices.js
@@ -1,29 +1,9 @@
-import APIServices from "./APIServices";
+import APIServices from "./common/APIServices";
 
-async function getProjectJiraThrowdNetServer() {
-	try {
-		let response = await APIServices.get("/JiraProject/GetAllProject");
-		return response;
-	} catch (error) {
-		return Promise.reject(error);
-	}
-}
-
-async function createProjectJiraThrowdNetServer() {
-	try {
-		let response = await APIServices.post("/JiraProject/CreateProject", {
-			id: 1,
-			name: "Project đó",
-		});
-		return response;
-	} catch (error) {
-		return Promise.reject(error);
-	}
-}
 
 async function getProjects(page) {
 	try {
-		let response = await APIServices.get(`/Projects/Index`, { page });
+		let response = await APIServices.get(`/api/Projects/GetAllProjects`, { page });
 		return response;
 	} catch (error) {
 		return Promise.reject(error);
@@ -31,7 +11,5 @@ async function getProjects(page) {
 }
 
 export {
-	getProjectJiraThrowdNetServer,
-	createProjectJiraThrowdNetServer,
 	getProjects,
 };
