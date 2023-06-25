4882d0d14959e7268cc41ec590f3c69e89a12e87
area1110 <khanhhuy1110@gmail.com>
2023-06-25 14:48:15 +0700

    Change authen verify method

--- src/resolvers/commonResolver.js
+++ src/resolvers/commonResolver.js
@@ -6,23 +6,44 @@ import { STORAGE } from "../common/constants";
  * @param {{ define: (arg0: string, arg1: { (req: any): Promise<any>; (req: any): Promise<any>; }) => void; }} resolver
  */
 function commonResolver(resolver) {
-  resolver.define(
-    "setContextToGlobal",
-    async (/** @type {{ context: any; }} */ req) => {
-      await storage.set(STORAGE.CONTEXT, req.context);
-      console.log(STORAGE.TOKEN, ": ", await storage.getSecret(STORAGE.TOKEN));
-      return req.context;
-    }
-  );
+	resolver.define("setContextToGlobal", async (req) => {
+		await storage.set(STORAGE.CONTEXT, req.context);
+		console.log(STORAGE.TOKEN, ": ", await storage.getSecret(STORAGE.TOKEN));
+		return req.context;
+	});
 
-  resolver.define("getAuthenUrl", async function (req) {
-    let authenUrl = await AuthenWithBE.generateOAuthURL(req.context);
-    let isAuthenticated = await storage.get("isAuthenticated");
-    return Promise.resolve({
-      isAuthenticated,
-      authenUrl,
-    });
-  });
+	resolver.define("getAuthenUrl", async function (req) {
+		try {
+			let authenUrl = await AuthenWithBE.generateOAuthURL(req.context);
+			let isAuthenticated = await storage.get("isAuthenticated");
+			return Promise.resolve({
+				isAuthenticated,
+				authenUrl,
+			});
+		} catch (error) {
+			return Promise.reject(error);
+		}
+	});
+
+	resolver.define("getIsAuthenticated", async function (req) {
+		try {
+			let isAuthenticated = await storage.get("isAuthenticated");
+			return Promise.resolve({
+				isAuthenticated,
+			});
+		} catch (error) {
+			return Promise.reject(error);
+		}
+	});
+
+	resolver.define("signout", async function (req) {
+		try {
+			await AuthenWithBE.handleUnauthorizedStatus();
+			return Promise.resolve();
+		} catch (error) {
+			return Promise.reject(error);
+		}
+	});
 }
 
 export default commonResolver;
