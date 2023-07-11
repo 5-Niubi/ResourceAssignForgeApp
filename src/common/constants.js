344386d7a04b7e30d20627ed7b464a8e875f910d
Đặng Tiến Đạt K15_HL <datdang912001@gmail.com>
2023-07-11 12:34:56 +0700

    Update constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,5 +1,5 @@
 export const EMPTY_STRING = "";
-export const BACKEND_dNET_DOMAIN = "https://629d-2401-d800-f22e-7f9e-f91f-7508-cf5d-ef13.ngrok-free.app";
+export const BACKEND_dNET_DOMAIN = "https://ddac-2405-4803-fc9b-96b0-a028-85aa-fa0a-9d3.ngrok-free.app";
  
 /**
  * Key of Storage
