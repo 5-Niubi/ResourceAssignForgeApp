dafc4d9ee67eb4128b543f0b77a94a5379052d0e
area1110 <khanhhuy1110@gmail.com>
2023-09-27 19:46:58 +0700

    Using localhost

--- src/common/environment.js
+++ src/common/environment.js
@@ -1,2 +1,2 @@
-export const BACKEND_SERVER_DOMAIN = "https://production-be.ai4cert.com";
+export const BACKEND_SERVER_DOMAIN = "https://cool-trams-dig.loca.lt";
 export const APP_CLIENT_ID = "wDzzxAZSrrM9DtPwZ295BMT3YoFR6KeD";
